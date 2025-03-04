ac0895440a252dd23bbb6dce757deedf45f31ac6
Szendefy Erik <szendefy.erik@gmail.com>
2025-03-04 15:52:50 +0100

    footer design and code updated and repaired, design somewhat finalized, responsivity is needed

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -5,41 +5,87 @@ import imgfacebook from "../../assets/footerImages/facebook_logo.png";
 import imginstagram from "../../assets/footerImages/instagram_logo.png";
 import imgtiktok from "../../assets/footerImages/tiktok_logo.png";
 import classes from "./footer.module.css";
+import norelaxlogo from "../../assets/NoRelaxLogo.png";    
+import locationIcon from "../../assets/footerImages/locationIcon.png";
+import phoneIcon from "../../assets/footerImages/phoneIcon.png";
+import emailIcon from "../../assets/footerImages/emailIcon.png";
+import { footer } from "framer-motion/client";
 
-function Footer() {
-    const {t} = useTranslation();
+function Footer () {
+const {t} = useTranslation();
+
     return (
-        <section className="footer">
-            <hr className="footer-separator"/>
-            <section className="footer-information">
-                <h2>{t('footer.information')}</h2>
-                <section className="footer-information-left">
-                    <section className="footer-information-address">
-                        <p>1183 Balassi Bálint utca 2-10</p>
-                    </section>
+      <section className={classes.footer}>
+        <hr className={classes["footer-seperator"]} />
+        
+        <section className={classes["footer-info"]}>
+          <section className={classes["footer-info-left"]}>
+            <div className={classes["footer-info__information"]}>
+                <h2>{t("footer.information")}</h2>
+            </div>
+          
+                <section className={classes["footer-info__icons"]}>
+                    <div>
+                        <div className={classes["footer-info-content-address"]}>
+                            <img src={locationIcon} alt="location" />
+                            <p>1183 Balassi Bálint utca 2-10</p>
+                        </div>
+                    </div>
+                    <div>
+                        <div className={classes["footer-info-content-phone"]}>
+                            <img src={phoneIcon} alt="phoneNumber" />
+                            <p>06 70 368 1515</p>
+                        </div>
+                    </div>
+                    <div>
+                        <div className={classes["footer-info-content-email"]}>
+                            <img src={emailIcon} alt="email_address" />
+                            <p>norelaxpub@gmail.com</p>
+                        </div>
+                    </div>
                     
-                    <p>06 70 368 1515</p>
-                    <p>norelaxpub@gmail.com</p>
-                    <p>{t('footer.policy')}</p>
+                    
+                    
                 </section>
-
+          </section>
+          <section className={classes["footer-info-center"]}>
+            <section>
+                <img className={classes["footer-logo"]} src={norelaxlogo} alt="norelax" />
             </section>
-            <hr className="footer-separator"/>    
-            <section className="footer-socialMedia">
-                <h2>{t('footer.socials')}</h2>
-                <section className="footer-socials">
-                    <img className={classes["icon"]} src={imgfacebook} alt="" />
-                    <a href="https://www.facebook.com/norelax?locale=hu_HU">Facebook</a>
-                    <img className={classes["icon"]} src={imginstagram} alt="" />
-                    <a href="https://www.facebook.com/norelax?locale=hu_HU">Facebook</a>
-                    <img className={classes["icon"]} src={imgtiktok} alt="" />
-                    <a href="https://www.facebook.com/norelax?locale=hu_HU">Facebook</a>
-
+          </section>
+          <section className={classes["footer-info-right"]}>
+            <section className={classes["footer-info__social-media"]}>
+                <h2>{t("footer.socials")}</h2>
+                <section className={classes["footer-info__social-media__icons"]}>
+                    <div>
+                        <div className={classes["footer-social-content-facebook"]}>
+                            <img src={imgfacebook} alt="facebook" />
+                            <p>NoRelax</p>
+                        </div>
+                    </div>
+                    <div>
+                        <div className={classes["footer-social-content-instagram"]}>
+                            <img src={imginstagram} alt="instagram" />
+                            <p>norelax_pub</p>
+                        </div>
+                    </div>
+                    <div>
+                        <div className={classes["footer-social-content-tiktok"]}>
+                            <img src={imgtiktok} alt="tiktok" />
+                            <p>norelax_pub</p>
+                        </div>
+                    </div>
+                    
+                    
+                    
                 </section>
             </section>
-            
+          </section>
         </section>
+        <hr className={classes["footer-seperator"]} />
+      </section>
     )
-}
-
-export default Footer;
+  
+  }
+  
+  export default Footer;
